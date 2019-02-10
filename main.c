e273a27c8b314492da2fab1f6f4b1b10d6211363
ytljc2003 <ytljc2003@gmail.com>
2019-02-09 20:14:56 -0600

    why

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+void update()
+{
+  printf("Let's see how to update!");
+}
 int main(int argc , char **argv )
 {
   printf("This is a test!\n");
+  update();
+
   return 0;
 }
