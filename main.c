d8a38e4d14ddaeda3e5d2d9cdad637163775d1b3
unknown <ytljc2003@gmail.com>
2019-02-13 11:10:18 -0600

    update version info

--- main.c
+++ main.c
@@ -4,9 +4,15 @@ void update()
 {
   printf("Let's see how to update!");
 }
+
+void version()
+{
+	printf("Version : 1.0.0\r\n");
+}
+
 int main(int argc , char **argv )
 {
-  printf("This is a test!\n");
+  printf("This is a test!\r\n");
   update();
 
   return 0;
